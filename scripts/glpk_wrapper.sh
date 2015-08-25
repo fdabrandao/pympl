308b3d285d582921231a388e7283a70101c0605c
Filipe Brandao <fdabrandao@gmail.com>
2015-08-25 02:45:08 +0100

    Add bash scripts

---
+++ scripts/glpk_wrapper.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+# This code is part of the PyMPL modelling extension.
+#
+# Copyright (C) 2015-2015, Filipe Brandao
+# Faculdade de Ciencias, Universidade do Porto
+# Porto, Portugal. All rights reserved. E-mail: <fdabrandao@dcc.fc.up.pt>.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+echo "Copyright (C) 2015-2015, Filipe Brandao"
+
+CMD="$0 $*"
+BASEDIR=`dirname $0`
+BIN_DIR=$BASEDIR/../bin/
+TMP_DIR=`mktemp -d -t XXXXXXXXXX`
+trap "rm -rf $TMP_DIR;" SIGHUP SIGINT SIGTERM EXIT
+
+usage(){
+    echo -e "Usage:"
+    echo -e "  $0 --mps/--lp model.mps/.lp"
+    echo -e "  $0 --mps/--lp model.mps/.lp --wsol vars.sol"
+}
+
+error(){
+    echo "Command line: "$CMD
+    echo "Error: invalid arguments."
+    usage
+    exit 1
+}
+
+solve(){
+    local model_file=$1
+    echo -e "\n>>> solving the MIP model using GLPK..."
+    echo -e "Note: different parameter settings may improve the performance substantially!"
+    if [[ $model_file =~ \.mps$ ]]; then
+        glpsol --mps $model_file $options -o $TMP_DIR/sol.out &
+        local pid=$!
+        trap "kill $pid &> /dev/null" SIGHUP SIGINT SIGTERM
+        wait $pid
+    else
+        glpsol --lp $model_file $options -o $TMP_DIR/sol.out &
+        local pid=$!
+        trap "kill $pid &> /dev/null" SIGHUP SIGINT SIGTERM
+        wait $pid
+    fi
+
+    sed -ni '/Column name/,/^$/p' $TMP_DIR/sol.out
+    sed -i '1,2d' $TMP_DIR/sol.out
+    sed -i ':a;N;$!ba;s/\n\s\{20\}/ /g' $TMP_DIR/sol.out
+    sed -i 's/\*/ /g' $TMP_DIR/sol.out
+    awk '{ if ( $3 ~ /^[0-9][^\s]*$/  ){ print $2, $3 }else{ print $2, $4 } } ' $TMP_DIR/sol.out > $TMP_DIR/vars.sol
+}
+
+options=""
+model_file=""
+sol_file=""
+
+while true;
+do
+  case "$1" in
+    --mps)
+        if [[ -n "$2" && -e "$2" && "$2" =~ \.mps$ ]]; then
+            model_file=$2
+        else
+            error
+        fi
+        shift 2;;
+
+    --lp)
+        if [[ -n "$2" && -e "$2" && "$2" =~ \.lp$ ]]; then
+            model_file=$2
+        else
+            error
+        fi
+        shift 2;;
+
+    --wsol)
+        if [[ -n "$2" ]]; then
+            sol_file=$2
+        else
+            error
+        fi
+        shift 2;;
+
+    --options)
+        if [[ -n "$2" ]]; then
+            options=$2
+        else
+            error
+        fi
+        shift 2;;
+
+    *)
+        if [[ -n "$1" ]]; then
+            error
+        else
+            break
+        fi
+  esac
+done
+
+if [[ -z "$model_file" ]]; then
+    error
+fi
+
+solve $model_file;
+
+if [[ -n "$sol_file" ]]; then
+    cp $TMP_DIR/vars.sol $sol_file
+fi
---
+++ scripts/scip_wrapper.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+# This code is part of the PyMPL modelling extension.
+#
+# Copyright (C) 2015-2015, Filipe Brandao
+# Faculdade de Ciencias, Universidade do Porto
+# Porto, Portugal. All rights reserved. E-mail: <fdabrandao@dcc.fc.up.pt>.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+echo "Copyright (C) 2015-2015, Filipe Brandao"
+
+CMD="$0 $*"
+BASEDIR=`dirname $0`
+BIN_DIR=$BASEDIR/../bin/
+TMP_DIR=`mktemp -d -t XXXXXXXXXX`
+trap "rm -rf $TMP_DIR;" SIGHUP SIGINT SIGTERM EXIT
+
+usage(){
+    echo -e "Usage:"
+    echo -e "  $0 --mps/--lp model.mps/.lp"
+    echo -e "  $0 --mps/--lp model.mps/.lp --wsol vars.sol"
+}
+
+error(){
+    echo "Command line: "$CMD
+    echo "Error: invalid arguments."
+    usage
+    exit 1
+}
+
+solve(){
+    local model_file=$1
+    echo -e "\n>>> solving the MIP model using SCIP..."
+    echo -e "Note: different parameter settings may improve the performance substantially!"
+    rm -rf $TMP_DIR/vars.sol;
+    (
+        echo "read $model_file"
+        echo $options
+        echo "optimize"
+        echo "write solution $TMP_DIR/vars.sol"
+    ) | scip &
+    local pid=$!
+    trap "kill $pid &> /dev/null" SIGHUP SIGINT SIGTERM
+    wait $pid
+    echo ""
+    tail -n+3 $TMP_DIR/vars.sol | awk '{ print $1, $2 }' > $TMP_DIR/vars.sol2
+    mv $TMP_DIR/vars.sol2 $TMP_DIR/vars.sol
+}
+
+options=""
+model_file=""
+model_file=""
+sol_file=""
+
+while true;
+do
+  case "$1" in
+    --mps)
+        if [[ -n "$2" && -e "$2" && "$2" =~ \.mps$ ]]; then
+            model_file=$2
+        else
+            error
+        fi
+        shift 2;;
+
+    --lp)
+        if [[ -n "$2" && -e "$2" && "$2" =~ \.lp$ ]]; then
+            model_file=$2
+        else
+            error
+        fi
+        shift 2;;
+
+    --wsol)
+        if [[ -n "$2" ]]; then
+            sol_file=$2
+        else
+            error
+        fi
+        shift 2;;
+
+    --options)
+        if [[ -n "$2" ]]; then
+            options=$2
+        else
+            error
+        fi
+        shift 2;;
+
+    *)
+        if [[ -n "$1" ]]; then
+            error
+        else
+            break
+        fi
+  esac
+done
+
+if [[ -z "$model_file" ]]; then
+    error
+fi
+
+solve $model_file;
+
+if [[ -n "$sol_file" ]]; then
+    cp $TMP_DIR/vars.sol $sol_file
+fi
