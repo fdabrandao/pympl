388f26c790b57e611be2713811a813759fdf251c
Filipe Brandao <fdabrandao@gmail.com>
2015-09-26 02:50:51 +0100

    Add Web App initial version

---
+++ pympl/webapp/app.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python
+"""
+This code is part of the Arc-flow Vector Packing Solver (VPSolver).
+
+Copyright (C) 2013-2015, Filipe Brandao
+Faculdade de Ciencias, Universidade do Porto
+Porto, Portugal. All rights reserved. E-mail: <fdabrandao@dcc.fc.up.pt>.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+from __future__ import print_function
+from builtins import str
+from builtins import object
+
+import os
+import sys
+
+DEBUG = False
+PORT = 5555
+
+if __name__ == "__main__":
+    sdir = os.path.dirname(__file__)
+    if sdir != "":
+        os.chdir(sdir)
+        __file__ = os.path.basename(__file__)
+        sys.argv[0] = __file__
+
+    if "test_install" in sys.argv:
+        sys.argv.remove("test_install")
+    else:
+        project_dir = "../../"
+        sys.path.insert(0, project_dir)
+        os.environ["PATH"] = "{0}/scripts:{1}".format(
+            project_dir, os.environ["PATH"]
+        )
+
+    if len(sys.argv) >= 2 and sys.argv[1].isdigit():
+        PORT = int(sys.argv[1])
+
+import flask
+import signal
+from flask import Flask, Response
+from flask import render_template, json, request, redirect, url_for
+from multiprocessing import Process
+from pympl import PyMPL, Solver
+
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.context_processor
+def inject_globals():
+    """Sends global data to templates."""
+    data = dict(
+        app_name="PyMPL App",
+        pages=[
+            ("/pympl", "PyMPL"),
+        ],
+    )
+    return data
+
+
+@app.route("/favicon.ico")
+def favicon():
+    """Favicon route."""
+    return flask.send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico", mimetype='image/vnd.microsoft.icon'
+    )
+
+
+@app.route("/")
+def index():
+    """Renders the index page."""
+    return redirect(url_for("pympl"))
+
+
+def load(fname):
+    """Loads a text file as a string."""
+    with open(fname, "r") as f:
+        return f.read().strip("\n")
+
+
+def request_data():
+    """Retrieves request contents."""
+    pympl_model = request.form["pympl_model"]
+    python_code = request.form["python_code"]
+    fnames = {}
+    contents = {}
+
+    for key in request.form:
+        if key.startswith("fname"):
+            ind = int(key.replace("fname", ""))
+            fnames[ind] = request.form[key]
+            if os.path.isabs(fnames[ind]) or ".." in fnames[ind]:
+                fnames[ind] = "invalid file name"
+        elif key.startswith("content"):
+            ind = int(key.replace("content", ""))
+            contents[ind] = request.form[key]
+
+    if "addfile" in request.form:
+        if fnames != {}:
+            next_ind = max(fnames)+1
+        else:
+            next_ind = 1
+        fnames[next_ind] = ""
+        contents[next_ind] = ""
+
+    files = [
+        (ind, fnames[ind], contents[ind])
+        for ind in sorted(fnames)
+    ]
+    return pympl_model, python_code, files
+
+
+@app.route("/pympl/", defaults={"example": None}, methods=["GET", "POST"])
+@app.route("/pympl/<example>", methods=["GET", "POST"])
+def pympl(example):
+    """Renders the input page."""
+    title = "PyMPL: AMPL extension"
+
+    example_folder = "data/examples/"
+    examples = {
+        "/pympl/": ("", "", None, []),
+        "/pympl/vbp": (
+            "Vector Packing",
+            ("vector_packing.mod", None, [])
+        ),
+        "/pympl/vsbpp": (
+            "Variable Sized Bin Packing",
+            ("variable_size_bin_packing.mod", None, [])
+        ),
+        "/pympl/pwl": (
+            "Piecewise Linear Function",
+            ("piecewise_linear.mod", None, [])
+        ),
+        "/pympl/ppbymip_cgp": (
+            "Consumer Goods Production",
+            ("ppbymip_cgp.mod", "ppbymip_cgp.py", ["data/cgpdemand.dat"])
+        )
+    }
+    examples_list = sorted(examples)
+
+    if request.method == "POST":
+        pympl_model, python_code, files = request_data()
+    else:
+        pympl_model = ""
+        python_code = ""
+        files = []
+        if example is not None:
+            modelf, codef, flist = examples["/pympl/"+example][1]
+            if modelf is not None:
+                pympl_model = open(example_folder+modelf).read()
+            if codef is not None:
+                python_code = open(example_folder+codef).read()
+            for ind, fname in enumerate(flist):
+                content = open(example_folder+fname).read()
+                files.append((ind+1, fname, content))
+
+    return render_template(
+        "input.html",
+        title=title,
+        examples=examples,
+        examples_list=examples_list,
+        pympl_model=pympl_model,
+        python_code=python_code,
+        files=files,
+        evaluator_url="/evaluate",
+    )
+
+
+class StringStream:
+    """Simple string stream."""
+    def __init__(self):
+        self.data = ""
+
+    def write(self, s):
+        self.data += s
+
+
+@app.route("/evaluate", methods=["POST"])
+def evaluate():
+    """Renders the evaluation page."""
+    import tempfile, shutil
+    from flask import make_response
+    org_cwd = os.getcwd()
+    tmp_dir = tempfile.mkdtemp()
+    os.chdir(tmp_dir)
+    try:
+        pympl_model, python_code, files = request_data()
+
+        with open("model.mod", "w") as f:
+            f.write(pympl_model)
+
+        for ind, path, contents in files:
+            path.replace(" ", "\\ ")
+            if path != "":
+                dname = os.path.dirname(path)
+                if dname != "" and not os.path.exists(dname):
+                    os.makedirs(dname)
+                with open(path, "w") as f:
+                    f.write(contents)
+
+        sys.stdout = StringStream()
+        exec(python_code, globals(), locals())
+        output = sys.stdout.data
+        sys.stdout = sys.__stdout__
+        os.chdir(org_cwd)
+        shutil.rmtree(tmp_dir)
+
+        if "download" in request.form:
+            response = make_response(output.decode("utf-8"))
+            header = "attachment; filename=output.mod"
+            response.headers["Content-Disposition"] = header
+            return response
+        else:
+            return output
+
+    except Exception as e:
+        sys.stdout = sys.__stdout__
+        os.chdir(org_cwd)
+        shutil.rmtree(tmp_dir)
+        raise
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=PORT, threaded=True)
---
+++ pympl/webapp/data/examples/ppbymip_cgp.py
@@ -0,0 +1,23 @@
+def read_table(fname, index1, index2, transpose=False):
+    """Reads a table from a file."""
+    if transpose:
+        index1, index2 = index2, index1
+    with open(fname) as f:
+        text = f.read().replace(",", "")
+        lst = list(map(float, text.split()))
+        demand = {}
+        for i1 in index1:
+            for i2 in index2:
+                if transpose:
+                    demand[i2, i1] = lst.pop(0)
+                else:
+                    demand[i1, i2] = lst.pop(0)
+        assert lst == []
+        return demand
+
+print("/*")
+pympl = PyMPL(locals_=locals(), globals_=globals())
+pympl.parse("model.mod")
+print("*/")
+
+print(pympl.output)
