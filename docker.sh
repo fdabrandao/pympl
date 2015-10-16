f23cf49b841d00afbd30f713070483ee94a8ebf8
Filipe Brandao <fdabrandao@gmail.com>
2015-10-16 16:49:33 +0100

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -6,4 +6,4 @@ docker build -t fdabrandao/pympl . || exit 1
 docker stop $(docker ps -a | grep fdabrandao/pympl | cut -d" " -f1) 2>/dev/null
 docker rm $(docker ps -a | grep fdabrandao/pympl | cut -d" " -f1) 2>/dev/null
 docker run -it --rm -p 5555 fdabrandao/pympl || exit 1
-#docker run -it --rm -p 5555 fdabrandao/pympl python -m pympl.webapp.app 5555 PASS
+#docker run -it --rm -p 5555 fdabrandao/pympl bash
