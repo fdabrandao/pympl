e91447fbe4e24cbd80ff4bec9fce8caa62fa3c82
Filipe Brandao <fdabrandao@gmail.com>
2016-04-26 23:10:45 +0100

    Add missing import

--- examples/test_ppbymip.py
+++ examples/test_ppbymip.py
@@ -21,6 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 from __future__ import print_function
 
+import sys
 import ppbymip_bike as bike
 import ppbymip_cgp as cgp
 import ppbymip_clb as clb
